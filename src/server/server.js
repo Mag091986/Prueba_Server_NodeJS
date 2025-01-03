32baf405032efaa4d7357c920d256a15684de1f7
Mag091986 <k9921star@gmail.com>
2025-01-02 21:17:15 -0500

    Creo un servidor para la app en Node y express

---
+++ src/server/server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const path = require('path')
+
+const startServer = (options) => {
+    const { port, public_path =  'public'} = options
+    
+    const app = express()
+
+    //Para usar middlewares poner use de express
+    app.use(express.static(public_path)) // Para páginas estáticas
+
+    app.get('*', (req, res) => {
+        const indexPath = path.join(__dirname + `../../../${public_path}/index.html`)
+        res.sendFile(indexPath)
+    })
+
+    app.listen(port, () => {
+        console.log(`Ejecutando puerto ${port}`)
+    })
+}
+
+module.exports = {
+    startServer
+}
